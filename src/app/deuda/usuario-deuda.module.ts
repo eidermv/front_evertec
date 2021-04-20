22e0fc053563c9168b4b2d0099abbbc37cbf3ab2
Eider Muñoz <eider.munoz@asmetsalud.com>
2021-04-19 21:20:01 -0500

    Configuracion inicial y creacion de modulo para usuario-deuda

---
+++ src/app/deuda/usuario-deuda.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UsuarioDeudaRoutingModule } from './usuario-deuda-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UsuarioDeudaRoutingModule
+  ]
+})
+export class UsuarioDeudaModule { }
